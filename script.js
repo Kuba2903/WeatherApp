a6bb41a429c5279b6005d663eeb683a96b516237
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-05-22 14:31:19 +0200

    initialized repository

---
+++ script.js
@@ -0,0 +1,11 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const cityName = document.querySelector('.city-name')
+const warning = document.querySelector('.warning')
+const photo = document.querySelector('.photo')
+const weather = document.querySelector('.weather')
+const temperature = document.querySelector('.temperature')
+const humidity = document.querySelector('.humidity')
+
+// https://openweathermap.org/
+// https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Web_mechanics/What_is_a_URL
