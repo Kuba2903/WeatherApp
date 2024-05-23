fb7e9d7a8ad52cbe9e0afa7fd1cca30f21c267a3
Kuba2903 <jakub.adamczyk@microsoft.wsei.edu.pl>
2024-05-23 12:31:55 +0200

    connect to api

--- script.js
+++ script.js
@@ -7,5 +7,42 @@ const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
 
+const APIkey = "063347538cb74b0c984a59afe0749fee";
+const city = "Krakow";
+
+button.addEventListener('click', () => {
+    const city = input.value;
+    if (city) {
+        const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}&units=metric`;
+        
+        axios.get(apiUrl)
+            .then(response => {
+                console.log(response);
+                const data = response.data;
+                const weatherData = data.list[0].main.temp;
+                const mainData = data.main;
+
+                cityName.textContent = data.city.name;
+                weather.textContent = weatherData;
+                temperature.textContent = `Temperature: ${weatherData}°C`;
+                humidity.textContent = `Humidity: %`;
+
+                warning.textContent = '';
+            })
+            .catch(error => {
+                warning.textContent = 'City not found. Please try again.';
+                cityName.textContent = '';
+                weather.textContent = '';
+                temperature.textContent = '';
+                humidity.textContent = '';
+                photo.src = '';
+                photo.alt = '';
+            });
+    } else {
+        warning.textContent = 'Please enter a city name.';
+    }
+});
+
+
 // https://openweathermap.org/
 // https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Web_mechanics/What_is_a_URL
